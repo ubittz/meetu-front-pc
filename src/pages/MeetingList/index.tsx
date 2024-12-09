70214433992fee71a4e96ad5cfcc9529c735c920
성현관 <uinus.shg@uinus.co.kr>
2024-12-09 16:57:43 +0900

    refactor: Page에 Header 컴포넌트 적용

--- src/pages/MeetingList/index.tsx
+++ src/pages/MeetingList/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import images from '@@assets/images';
 import Footer from '@@components/Footer';
+import Header from '@@components/Header';
 import { PAGES } from '@@router/constants';
 import { pathGenerator } from '@@router/utils';
 
@@ -10,44 +11,7 @@ import { pathGenerator } from '@@router/utils';
 function MeetingList() {
   return (
     <div id='wrap'>
-      <header className='header'>
-        {
-          <div className='inner'>
-            <div className='inner_l'>
-              <h1 className='logo'>
-                <Link to={pathGenerator(PAGES.MAIN)}>meetu</Link>
-              </h1>
-              <nav id='navi'>
-                <ul>
-                  <li>
-                    <Link to={pathGenerator(PAGES.MEETING)}>모임 탐색</Link>
-                  </li>
-                  <li>
-                    <Link to='javascript:void(0);'>호스트 소개</Link>
-                  </li>
-                  <li>
-                    <Link to='javascript:void(0);'>공지사항</Link>
-                  </li>
-                  <li className='active'>
-                    <Link to={pathGenerator(PAGES.MEETING) + '/create'}>모임 개설하기</Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div className='inner_r'>
-              <ul>
-                <li className='logout'>
-                  <Link to='javascript:void(0);'>로그아웃</Link>
-                </li>
-                <li className='mypage'>
-                  <Link to='javascript:void(0);'>마이페이지</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        }
-      </header>
-
+      <Header />
       <main className='container'>
         <section className='meeting_visual'>
           <div className='mv_inner'>
