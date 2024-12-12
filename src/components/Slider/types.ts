3e0be35ff5e5bd9ea7ef76a019a091ffa40d4927
fire-irror <firenoah.m@gmail.com>
2024-12-12 16:20:24 +0900

    feat: slider 구현

--- src/components/Slider/types.ts
+++ src/components/Slider/types.ts
@@ -2,9 +2,10 @@ import { HTMLAttributes, ReactNode } from 'react';
 
 import { Settings } from 'react-slick';
 
+// 기존 SliderProps 확장하여 onSlideChange 추가
 export interface SliderProps extends HTMLAttributes<HTMLDivElement> {
-  sliderProps?: Settings;
-  items: ReactNode[];
-  gap?: number;
-  itemSize?: number;
-}
+  sliderProps?: Settings; 
+  items: ReactNode[]; 
+  gap?: number; 
+  itemSize?: number; 
+  onSlideChange?: (index: number) => void; }
