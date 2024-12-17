ed72748854c24b4f83a0d601327c86709f8f656b
fire-irror <firenoah.m@gmail.com>
2024-12-17 16:27:12 +0900

    fix: slider 진행바

--- src/components/Slider/SliderNavigation.tsx
+++ src/components/Slider/SliderNavigation.tsx
@@ -3,17 +3,11 @@ import styled from 'styled-components';
 import { COLORS } from '@@constants/colors';
 
 const StyledSliderNavigation = styled.div`
-  width: 70%; 
-  height: 3px; 
-  background: ${COLORS.BORDER};
-  position: relative;
-  margin-top: 40px;
-
   width: 78%;
   height: 2px;
   background: ${COLORS.BORDER};
   position: relative;
-  margin-top:60px;
+  margin-top: 60px;
   border-radius: 3px;
 `;
 
@@ -21,14 +15,14 @@ const StyledActiveLine = styled.div<{ width: number }>`
   height: 100%;
   background: ${COLORS.MAIN};
   border-radius: 3px;
-  transition: width 0.3s ease; 
+  transition: width 0.3s ease;
   width: ${({ width }) => width}%;
 `;
 
 function SliderNavigation({ currentIndex, length }: { currentIndex: number; length: number }) {
-  const initialProgress = (currentIndex === 0 ? 1 : currentIndex); 
+  const defaultProgress = 20; 
 
-  const progress = (initialProgress / (length - 1)) * 100;
+  const progress = currentIndex === 0 ? defaultProgress : (currentIndex / (length - 1)) * (100 - defaultProgress) + defaultProgress;
 
   return (
     <StyledSliderNavigation>
