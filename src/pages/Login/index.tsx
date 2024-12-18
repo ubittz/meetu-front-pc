597de26958142f8e09a53a499615803a310b0034
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-12-18 11:24:18 +0900

    feat: set login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,11 @@
-import { Form, Formik } from 'formik';
-import { Field } from 'formik';
+import { Formik } from 'formik';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
 import Footer from '@@components/Footer';
 import Header from '@@components/Header';
+import { loginSchema } from '@@constants/scheme';
+import LoginFormContent from '@@pages/Login/part/LoginFormContent';
 import { LoginForm } from '@@pages/Login/types';
 import { PAGES } from '@@router/constants';
 import { pathGenerator } from '@@router/utils';
@@ -37,45 +38,17 @@ function Login() {
       alert(payload);
     },
   });
+
   return (
     <div id='wrap'>
       <Header />
-
       <main className='container'>
         <div className='member_inner'>
           <h2>로그인</h2>
           <p className='caption'>밋 유에 오신 것을 환영합니다.</p>
-
-          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            <Form>
-              <fieldset>
-                <legend>로그인 영역</legend>
-                <div className='input_wrap'>
-                  <div className='input_area' id='id'>
-                    <Field type='text' name='id' id='id' placeholder='아이디를 입력해주세요.' />
-                  </div>
-                  <div className='input_area' id='password'>
-                    <Field type='password' name='password' id='password' placeholder='비밀번호를 입력해주세요.' />
-                  </div>
-                </div>
-
-                <div className='btn_area'>
-                  <button type='submit' className='btn'>
-                    로그인하기
-                  </button>
-                </div>
-                <div className='btn_area btn_sub'>
-                  <Link to={pathGenerator(PAGES.LOGIN) + '/find/id'} className='btn'>
-                    아이디 찾기
-                  </Link>
-                  <Link to={pathGenerator(PAGES.LOGIN) + '/find/password'} className='btn'>
-                    비밀번호 찾기
-                  </Link>
-                </div>
-              </fieldset>
-            </Form>
+          <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={loginSchema}>
+            <LoginFormContent />
           </Formik>
-
           <div className='btn_join'>
             <p className='or_txt'>
               <span>or</span>
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
+import auth from '@@stores/auth/reducer';
 import home from '@@stores/home/reducer';
+import meeting from '@@stores/meeting/reducer';
 
-export const rootReducers = combineReducers({ home });
+export const rootReducers = combineReducers({ auth, home, meeting });
