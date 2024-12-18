3c227c8a7cb3ffa1e583700d00e72046733212d2
samusesapple <uinus.shg@uinus.co.kr>
2024-12-18 15:09:53 +0900

    feat: add API endpoints

--- src/utils/request/constants.ts
+++ src/utils/request/constants.ts
@@ -1,5 +1,42 @@
 export const API_ENDPOINT = import.meta.env.VITE_APP_API_ENDPOINT;
 
+export const ENDPOINTS = {
+  // 유저 & Auth
+  USER: {
+    BASE: `/user`,
+    CHECK_ID: `/user/check-id`,
+    CHECK_EMAIL: `/user/check-email`,
+    REGISTER: `/user/register`,
+    EDIT: `/user/edit`,
+    GET_MY_INFO: `/user/get-my-info`,
+    FIND_ID: `/user/find-id`,
+  },
+  // 모임
+  MEETING: {
+    BASE: `/meeting`,
+    DETAILS: (id: string) => `/meeting/${id}`, // get
+    MY_CONTENTS: `/meeting/my-contents`, // get
+    LIST: `/meeting/list`, // get
+    LIST_LAST_MONTH: `/meeting/list-last-month`, // get
+    EDIT: `/meeting/edit`, // patch
+    ADD: `/meeting/add`, // put
+  },
+  // 문의
+  CONTACT: {
+    BASE: `/meeting/contact`,
+    LIST: `/meeting/contact/list`, // get
+    ADD: `/meeting/contact/add`, // put
+  },
+  // 결제
+  PAYMENT: {
+    BASE: `/payment`,
+    LIST: `/payment/list`, // get
+    GET_REST_ID: `/payment/get-rest-id`, // get - bootpay 호출 시 던져줄 ID
+    DETAIL: (id: string) => `/payment/detail/${id}`, // get
+    ADD: `/payment/add`, // put - bootpay 요청 시 사용할 config
+  },
+};
+
 export const ERROR_CODE = {
   USER_NOTFOUND: 'User_NotFound',
   PASSWORD_MISMATCH: 'Password_Mismatch',
