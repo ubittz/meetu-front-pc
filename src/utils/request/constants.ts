77d683b80f2d4ad22f8bea6ef325be4455d85491
samusesapple <uinus.shg@uinus.co.kr>
2024-12-18 15:11:28 +0900

    fix: fix api routes

--- src/utils/request/constants.ts
+++ src/utils/request/constants.ts
@@ -3,37 +3,37 @@ export const API_ENDPOINT = import.meta.env.VITE_APP_API_ENDPOINT;
 export const ENDPOINTS = {
   // 유저 & Auth
   USER: {
-    BASE: `/user`,
-    CHECK_ID: `/user/check-id`,
-    CHECK_EMAIL: `/user/check-email`,
-    REGISTER: `/user/register`,
-    EDIT: `/user/edit`,
-    GET_MY_INFO: `/user/get-my-info`,
-    FIND_ID: `/user/find-id`,
+    BASE: `/api/user`,
+    CHECK_ID: `/api/user/check-id`,
+    CHECK_EMAIL: `/api/user/check-email`,
+    REGISTER: `/api/user/register`,
+    EDIT: `/api/user/edit`,
+    GET_MY_INFO: `/api/user/get-my-info`,
+    FIND_ID: `/api/user/find-id`,
   },
   // 모임
   MEETING: {
-    BASE: `/meeting`,
-    DETAILS: (id: string) => `/meeting/${id}`, // get
-    MY_CONTENTS: `/meeting/my-contents`, // get
-    LIST: `/meeting/list`, // get
-    LIST_LAST_MONTH: `/meeting/list-last-month`, // get
-    EDIT: `/meeting/edit`, // patch
-    ADD: `/meeting/add`, // put
+    BASE: `/api/meeting`,
+    DETAILS: (id: string) => `/api/meeting/${id}`, // get
+    MY_CONTENTS: `/api/meeting/my-contents`, // get
+    LIST: `/api/meeting/list`, // get
+    LIST_LAST_MONTH: `/api/meeting/list-last-month`, // get
+    EDIT: `/api/meeting/edit`, // patch
+    ADD: `/api/meeting/add`, // put
   },
   // 문의
   CONTACT: {
-    BASE: `/meeting/contact`,
-    LIST: `/meeting/contact/list`, // get
-    ADD: `/meeting/contact/add`, // put
+    BASE: `/api/meeting/contact`,
+    LIST: `/api/meeting/contact/list`, // get
+    ADD: `/api/meeting/contact/add`, // put
   },
   // 결제
   PAYMENT: {
-    BASE: `/payment`,
-    LIST: `/payment/list`, // get
-    GET_REST_ID: `/payment/get-rest-id`, // get - bootpay 호출 시 던져줄 ID
-    DETAIL: (id: string) => `/payment/detail/${id}`, // get
-    ADD: `/payment/add`, // put - bootpay 요청 시 사용할 config
+    BASE: `/api/payment`,
+    LIST: `/api/payment/list`, // get
+    GET_REST_ID: `/api/payment/get-rest-id`, // get - bootpay 호출 시 던져줄 ID
+    DETAIL: (id: string) => `/api/payment/detail/${id}`, // get
+    ADD: `/api/payment/add`, // put - bootpay 요청 시 사용할 config
   },
 };
 
