4bda959bfc40b610672d42d57306052ad5873fad
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-12-23 16:38:28 +0900

    feat: add fech

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import Error from '@@pages/Error';
@@ -25,8 +28,20 @@ import JoinComplete from '@@pages/Register/parts/JoinComplete';
 import UserInfoInput from '@@pages/Register/parts/UserInfoInput';
 import { PAGES, ROUTE_PREFIX } from '@@router/constants';
 import { pathGenerator } from '@@router/utils';
+import { useAppState } from '@@store/hooks';
+import { fetchMeRequest } from '@@stores/auth/reducer';
 
 function Router() {
+  const dispatch = useDispatch();
+
+  const { me, token } = useAppState((state) => state.auth);
+
+  useEffect(() => {
+    if (token && !me) {
+      dispatch(fetchMeRequest());
+    }
+  }, [me, token, dispatch]);
+
   return (
     <Routes>
       <Route path='/' element={<Navigate to={ROUTE_PREFIX} />} />
